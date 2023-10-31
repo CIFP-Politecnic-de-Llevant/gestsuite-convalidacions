1639b08368636c0f56472951a4b0d5eb9a4048cf
Joan Galmés Riera <joan.galmes@gmail.com>
2023-10-31 20:02:23 +0100

    core Address depen del properties

--- src/main/java/cat/politecnicllevant/convalidacions/controller/SolicitudController.java
+++ src/main/java/cat/politecnicllevant/convalidacions/controller/SolicitudController.java
@@ -30,6 +30,11 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
@@ -38,15 +43,18 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
@@ -632,7 +640,7 @@ public class SolicitudController {
                 //        .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build()))
                 //        .build()) {
 
-                SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null,
+               /* SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null,
                                 new TrustStrategy() {
                                     @Override
                                     public boolean isTrusted(final X509Certificate[] chain, final String authType)
@@ -657,7 +665,45 @@ public class SolicitudController {
                         }
                         return out;
                     });
+                }*/
+
+                //final HttpGet getMethod = new HttpGet(HOST_WITH_SSL);
+
+                final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+                final SSLContext sslContext = SSLContexts.custom()
+                        .loadTrustMaterial(null, acceptingTrustStrategy)
+                        .build();
+                final SSLConnectionSocketFactory sslsf =
+                        new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+                final Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                        RegistryBuilder.<ConnectionSocketFactory> create()
+                                .register("https", sslsf)
+                                .register("http", new PlainConnectionSocketFactory())
+                                .build();
+
+                final BasicHttpClientConnectionManager connectionManager =
+                        new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
+                try( CloseableHttpClient httpClient = HttpClients.custom()
+                        .setConnectionManager(connectionManager)
+                        .build()){
+
+                         remotePath = httpClient.execute(httpPost, response -> {
+                        //do something with response
+                        System.out.println("Response de cridada 3...");
+                        System.out.println(response.getStatusLine().getStatusCode());
+                        System.out.println(response.getStatusLine().getReasonPhrase());
+                        InputStream responseInputStream = response.getEntity().getContent();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(responseInputStream));
+                        String line = "";
+                        String out = "";
+                        while ((line = bufferedReader.readLine()) != null) {
+                            out += line;
+                        }
+                        return out;
+                     });
                 }
+
                 System.out.println("Remote path"+remotePath);
 
 
